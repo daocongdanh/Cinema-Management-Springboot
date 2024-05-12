95ffb7e54a3f92b1a0b3edf365f36c1caed7230a
daocongdanh <daocongdanh47@gmail.com>
2024-05-12 10:10:53 +0700

    Thêm mới exception trong GlobalExceptionHandler và sửa xong ProductController

--- src/main/java/com/example/cinemamanagement/dtos/ProductDTO.java
+++ src/main/java/com/example/cinemamanagement/dtos/ProductDTO.java
@@ -1,6 +1,7 @@
 package com.example.cinemamanagement.dtos;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,14 +19,13 @@ public class ProductDTO {
     private String productName;
 
     @NotNull(message = "price must be not null")
-    @Min(value = 1, message="Price must be greater than or equal to 1")
+    @Min(value = 1, message="price must be greater than or equal to 1")
     private double price;
 
     @NotNull(message = "unitStock must be not null")
     @Min(value = 1, message="unitStock must be greater than or equal to 1")
     private int unitStock;
 
-    @NotNull(message = "status must be not null")
     private boolean status;
 
     @NotNull(message = "categoryId must be not null")
